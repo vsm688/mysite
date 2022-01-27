d2ba1c2278768136e5ec32986bfb4e97b3d3ef07
vsm688 <vakhoshvili@gmail.com>
2022-01-27 13:02:24 +1300

    added styling to main body fixed z-index issue

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 const toggle_menu = document.getElementById("togglemenubutton");
 const show_items = document.getElementById("mobilemenuitems");
 
-toggle_menu.addEventListener('click', () => {
-    show_items.classList.toggle('active');
-});
+
+
+const showContact = () =>{
+    document.getElementById("contactform").style.display ="block";
+    document.getElementById("topdiv").style.display ="none";
+    document.getElementById("botdiv").style.display ="none";
+
+}
+
+const showtext = () =>{
+    document.getElementById("contactform").style.display ="none";
+    document.getElementById("topdiv").style.display ="block";
+    document.getElementById("botdiv").style.display ="block";
+}
+
+window.onload = showtext;
